34fd0409a870cb47042f1d3dee7f731c6dd9aae9
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-01-17 05:33:06 -0600

    Adds initial state of the spring boot application

---
+++ src/main/groovy/org/jvmx/restdocs/jvmxspringrestdocs/JvmxSpringRestdocsApplication.groovy
@@ -0,0 +1,12 @@
+package org.jvmx.restdocs.jvmxspringrestdocs
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JvmxSpringRestdocsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run JvmxSpringRestdocsApplication, args
+	}
+}
