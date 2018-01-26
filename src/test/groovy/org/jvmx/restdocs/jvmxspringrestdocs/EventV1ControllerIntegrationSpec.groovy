416ee4f08ca381031cddfc50c8d46756e780170a
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-01-26 00:59:25 -0600

    Cleans event integration spec code

--- src/test/groovy/org/jvmx/restdocs/jvmxspringrestdocs/EventV1ControllerIntegrationSpec.groovy
+++ src/test/groovy/org/jvmx/restdocs/jvmxspringrestdocs/EventV1ControllerIntegrationSpec.groovy
@@ -10,14 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.restdocs.JUnitRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 
-@ContextConfiguration
 @SpringBootTest
 class EventV1ControllerIntegrationSpec extends Specification {
 
