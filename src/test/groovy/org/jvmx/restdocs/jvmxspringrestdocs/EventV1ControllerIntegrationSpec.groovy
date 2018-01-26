91dc12a9ed5e38e615bb28070ba254f8701eca62
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-01-26 00:56:32 -0600

    Adds spring restdocs to the project and the first integration test

---
+++ src/test/groovy/org/jvmx/restdocs/jvmxspringrestdocs/EventV1ControllerIntegrationSpec.groovy
@@ -0,0 +1,48 @@
+package org.jvmx.restdocs.jvmxspringrestdocs
+
+import static org.springframework.http.MediaType.APPLICATION_JSON
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+import org.junit.Rule
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.restdocs.JUnitRestDocumentation
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import spock.lang.Specification
+
+@ContextConfiguration
+@SpringBootTest
+class EventV1ControllerIntegrationSpec extends Specification {
+
+  @Rule
+  public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation()
+
+  private MockMvc mockMvc
+
+  @Autowired
+  private WebApplicationContext context
+
+  void setup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context)
+      .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentation))
+      .build()
+  }
+
+  def 'get a list of events'() {
+    when:
+      ResultActions result = mockMvc
+        .perform(get('/v1/events').accept(APPLICATION_JSON))
+
+    then:
+      result
+        .andExpect(status().isOk())
+        .andDo(document('get-eventsV1'))
+  }
+}
--- src/test/groovy/org/jvmx/restdocs/jvmxspringrestdocs/TastingSpec.groovy
+++ src/test/groovy/org/jvmx/restdocs/jvmxspringrestdocs/TastingSpec.groovy
@@ -1,10 +1,23 @@
 package org.jvmx.restdocs.jvmxspringrestdocs
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 
+@SpringBootTest
 class TastingSpec extends Specification {
+
+  @Autowired
+  WebApplicationContext context
+
   def 'should taste Spock'() {
     expect:
       2 + 2 == 4
+  }
+
+  def 'should test spring context'() {
+    expect: 'Spring context exists'
+      context != null
   }
 }
