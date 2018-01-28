f5cacdd589097b293ce6d0648bb420f03bc2927d
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-01-28 02:59:42 -0600

    Fixes id generic class

--- src/main/groovy/org/jvmx/restdocs/jvmxspringrestdocs/persistence/repositories/EventsRepository.groovy
+++ src/main/groovy/org/jvmx/restdocs/jvmxspringrestdocs/persistence/repositories/EventsRepository.groovy
@@ -3,5 +3,5 @@ package org.jvmx.restdocs.jvmxspringrestdocs.persistence.repositories
 import org.jvmx.restdocs.jvmxspringrestdocs.persistence.model.Event
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EventsRepository extends JpaRepository<Event, long> {
+interface EventsRepository extends JpaRepository<Event, Long> {
 }
